77b676bab4e552b765d46527090702ff1357b077
klibrass <dungphong602@gmail.com>
2025-08-05 14:48:56 +0800

    final
    hey

--- src/main.js
+++ src/main.js
@@ -1,13 +1,46 @@
+let isWandering = false;
+
 function setup() {
   createCanvas(1000, 400);
   vehicle = new Vehicle(width * 0.5, height * 0.5);
+
+  toggleWanderButton = createButton("Toggle Wander");
 }
 
 function draw() {
   background(220);
   let mouse = createVector(mouseX, mouseY);
 
-  vehicle.seek(mouse);
+  push();
+  stroke(5);
+  fill(0, 200, 0, 100);
+  circle(mouseX, mouseY, 50);
+  pop();
+
+  toggleWanderButton.mousePressed(toggleWander);
+
+  if (!isWandering) {
+    vehicle.seek(mouse);
+  } else {
+    vehicle.wander();
+  }
   vehicle.update();
   vehicle.show();
+  vehicle.outOfBounds();
+
+  showDebug();
+}
+
+function toggleWander() {
+  isWandering = !isWandering;
+}
+
+function showDebug() {
+  push();
+  textSize(20);
+  noStroke();
+  fill(0);
+  text('Speed: ' + round(vehicle.velocity.mag(), 2), 10, 25);
+  text('Is Wandering? ' + isWandering, 10, 50);
+  pop();
 }
--- src/vehicle.js
+++ src/vehicle.js
@@ -6,6 +6,7 @@ class Vehicle {
         this.size = 8;
         this.maxSpeed = 5.35;
         this.maxForce = 0.15;
+        this.wanderTheta = 0.0;
     }
 
     update() {
@@ -35,6 +36,31 @@ class Vehicle {
         this.applyForce(steer);
     }
 
+    wander() {
+        let wanderRadius = 25;
+        let wanderDistance = 80;
+        let wanderDelta = 0.3;
+
+        this.wanderTheta += random(-wanderDelta, wanderDelta);
+
+        let circlePos = this.velocity.copy();
+        circlePos.normalize();
+        circlePos.mult(wanderDistance);
+        circlePos.add(this.position);
+
+        let h = this.velocity.heading();
+
+        let circleOffSet = createVector(
+            wanderRadius * cos(this.wanderTheta + h),
+            wanderRadius * sin(this.wanderTheta + h)
+        );
+    
+        let target = p5.Vector.add(circlePos, circleOffSet);
+        this.seek(target);
+
+        this.drawWander(this.position, circlePos, target, wanderRadius);
+    }
+
     show() {
         let angle = this.velocity.heading();
 
@@ -49,5 +75,22 @@ class Vehicle {
         vertex(-this.size * 2, this.size);
         endShape(CLOSE);
         pop();
+    }
+
+    drawWander(location, circlePos, target, rad) {
+        stroke(0);
+        noFill();
+        strokeWeight(1);
+        circle(circlePos.x, circlePos.y, rad * 2);
+        circle(target.x, target.y, 4);
+        line(location.x, location.y, circlePos.x, circlePos.y);
+        line(circlePos.x, circlePos.y, target.x, target.y);
+    }
+
+    outOfBounds() {
+        if (this.position.x < -this.size) this.position.x = width + this.size;
+        if (this.position.y < -this.size) this.position.y = height + this.size;
+        if (this.position.x > width + this.size) this.position.x = -this.size;
+        if (this.position.y > height + this.size) this.position.y = -this.size;
     }
 }
